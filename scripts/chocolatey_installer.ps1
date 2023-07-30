139399075312d6723aad0d3edd411cae785747a3
Lucas Gabriel (lucmsilva) <lucmsilva651@gmail.com>
2023-07-29 22:23:03 -0300

    Update Chocolatey installer, fix bugs

--- scripts/chocolatey_installer.ps1
+++ scripts/chocolatey_installer.ps1
@@ -1,6 +1,4 @@
-cd dependency_files
-Add-AppxPackage -Path 8wekyb3d8bbwe.msixbundle
-cd ..
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 cd ..
 cd modules
 ./other_depredirect.cmd
