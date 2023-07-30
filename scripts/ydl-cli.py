4fad8b20cd4641c53ea7267551de8fb796c43e43
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-30 16:48:38 -0300

    Changes: Update in updater github

--- scripts/ydl-cli.py
+++ scripts/ydl-cli.py
@@ -5,19 +5,39 @@ import asyncio
 from yt_dlp import YoutubeDL
 from functools import wraps, partial
 from typing import Callable
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
 
 def updater():
     print("Checking for updates...")
     print()
-    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-    output = process.communicate()[0]
-    string = str(output)
-    if "Already up to date." in string:
-        print("ydl-cli Already in the latest version.")
-        print()
-    else:
-        print("ydl-cli Updated successfully!")
-        print()
+    try:
+        repo = Repo()
+    except GitCommandError:
+        return print("<i>Invalid Git Command</i>")
+     except InvalidGitRepositoryError:
+        repo = Repo.init()
+        if "upstream" in repo.remotes:
+            origin = repo.remote("upstream")
+        else:
+            origin = repo.create_remote("upstream", REPO_)
+        origin.fetch()
+        repo.create_head(BRANCH_, origin.refs.master)
+        repo.heads.master.set_tracking_branch(origin.refs.master)
+        repo.heads.master.checkout(True)
+    if repo.active_branch.name != BRANCH_:
+        return print("An error ocurred!")
+    try:
+        repo.create_remote("upstream", REPO_)
+    except BaseException:
+        pass
+    ups_rem = repo.remote("upstream")
+    ups_rem.fetch(BRANCH_)
+    try:
+        ups_rem.pull(BRANCH_)
+    except GitCommandError:
+        repo.git.reset("--hard", "FETCH_HEAD")
+    print("[ydl-cli] Updated Sucessfully!")
     
 
 def aiowrap(func: Callable) -> Callable:
@@ -47,7 +67,6 @@ async def download_youtube_video(video_url):
 
 async def main():
     print("[ydl-cli] Made by Lucas Gabriel (lucmsilva)")
-    updater()
     print("[ydl-cli] See new updates at: https://github.com/ydl-team/ydl-cli/")
     print()
     video_url = input("[ydl-cli] Enter the YouTube video URL or ID you want to download: ")
@@ -55,4 +74,5 @@ async def main():
     video_filename = await download_youtube_video(video_url)
 
 if __name__ == "__main__":
+    updater()
     asyncio.run(main())
