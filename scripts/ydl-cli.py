fe9ba4bd1dd7bb7f72b7f9bc326c9cfe0421b436
Lucas Gabriel (lucmsilva) <lucmsilva651@gmail.com>
2023-07-29 22:46:11 -0300

    Updated code

--- scripts/ydl-cli.py
+++ scripts/ydl-cli.py
@@ -20,23 +20,13 @@ def download_youtube_video(video_url):
 
     return video_filename
 
-def combine_audio_video(video_filename):
-    output_filename = os.path.splitext(video_filename)[0] + "_ydl_cli.mp4"
-    ffmpeg_cmd = ['ffmpeg', '-i', video_filename, '-i', video_filename.replace('.mp4', '.m4a'), '-c', 'copy', output_filename]
-    subprocess.run(ffmpeg_cmd)
-    os.remove(video_filename)
-    os.remove(video_filename.replace('.mp4', '.m4a'))
-
-    print(f"File saved as: {output_filename}")
-
 def main():
-    print("[ydl-cli] Made by Lucas Gabriel (lucmsilva) - Only runs on Windows")
-    print("[ydl-cli] See new updates at:")
-    print("[ydl-cli] https://github.com/ydl-team/ydl-cli/")
+    print("[ydl-cli] Made by Lucas Gabriel (lucmsilva)")
+    print("[ydl-cli] See new updates at: https://github.com/ydl-team/ydl-cli/")
     print()
     video_url = input("[ydl-cli] Enter the YouTube video URL or ID you want to download: ")
+    print()
     video_filename = download_youtube_video(video_url)
-    combine_audio_video(video_filename)
 
 if __name__ == "__main__":
     main()
