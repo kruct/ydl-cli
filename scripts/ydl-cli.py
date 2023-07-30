cc416952fa95a1072bf53c005a5bbb7d3ad5f3ab
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-30 15:11:10 -0300

    Checking for updates

--- scripts/ydl-cli.py
+++ scripts/ydl-cli.py
@@ -6,6 +6,20 @@ from yt_dlp import YoutubeDL
 from functools import wraps, partial
 from typing import Callable
 
+def updater():
+    print("Checking for updates...")
+    print()
+    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    string = str(output)
+    if "Already up to date." in string:
+        print("ydl-cli Already in the latest version.")
+        print()
+    else:
+        print("ydl-cli Updated successfully!")
+        print()
+    
+
 def aiowrap(func: Callable) -> Callable:
     @wraps(func)
     async def run(*args, loop=None, executor=None, **kwargs):
@@ -33,6 +47,7 @@ async def download_youtube_video(video_url):
 
 async def main():
     print("[ydl-cli] Made by Lucas Gabriel (lucmsilva)")
+    updater()
     print("[ydl-cli] See new updates at: https://github.com/ydl-team/ydl-cli/")
     print()
     video_url = input("[ydl-cli] Enter the YouTube video URL or ID you want to download: ")
